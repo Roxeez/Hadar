c2e6a2a24e3b693866442ddf8b899f9497840a9e
Roxeez <roxeezthe2nd@gmail.com>
2021-06-27 22:40:01 +0200

    Add finish

---
+++ Hadar/Assets/Scripts/End.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class End : MonoBehaviour
+{
+    private Animator animator;
+
+    private static readonly int FinishedAnimatorId = Animator.StringToHash("finished");
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool(FinishedAnimatorId, true);
+        }
+    }
+}
